472856f1cd13b87feee912602e8f809882ade2a9
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-08-10 09:12:44 -0500

    Módulo Instrumento, capa Domain

---
+++ Code/Organizador PEC-6-60/src/Instrumento/Domain/Exceptions/InvalidNombreInstrumento.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Organizador_PEC_6_60.Instrumento.Domain.Exceptions;
+
+public class InvalidNombreInstrumento : Exception
+{
+    public InvalidNombreInstrumento() : base("El nombre del instrumento es inválido.") {}
+}
---
+++ Code/Organizador PEC-6-60/src/Instrumento/Domain/ValueObjects/InstrumentoNombre.cs
@@ -0,0 +1,12 @@
+﻿namespace Organizador_PEC_6_60.Instrumento.Domain.ValueObjects
+{
+    public class InstrumentoNombre
+    {
+        public string Value { get; }
+
+        public InstrumentoNombre(string nombre)
+        {
+            Value = nombre;
+        }
+    }
+}
