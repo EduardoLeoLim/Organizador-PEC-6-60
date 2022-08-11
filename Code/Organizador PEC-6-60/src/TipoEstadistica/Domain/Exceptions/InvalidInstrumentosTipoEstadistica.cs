e828ca73f09ff53d5d4e28fbe77a45e4b6829b92
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-08-11 03:42:53 -0500

    Módulo Tipo Estadística, capa Domain. Excepción agregada

---
+++ Code/Organizador PEC-6-60/src/TipoEstadistica/Domain/Exceptions/InvalidInstrumentosTipoEstadistica.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Organizador_PEC_6_60.TipoEstadistica.Domain.Exceptions
+{
+    public class InvalidInstrumentosTipoEstadistica : Exception
+    {
+        public InvalidInstrumentosTipoEstadistica() : base("La lista de instrumentos en inválida") {}
+        
+        public InvalidInstrumentosTipoEstadistica(string errorMessage) : base(errorMessage) {}
+    }
+}
