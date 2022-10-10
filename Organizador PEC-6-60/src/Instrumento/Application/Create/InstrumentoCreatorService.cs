122facf46660a06782b55d6f1d17427a57304cd3
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-09 23:46:35 -0500

    refactor(instrumento): desacoplando caso de uso para registrar instrumento

---
+++ Organizador PEC-6-60/src/Instrumento/Application/Create/InstrumentoCreatorService.cs
@@ -0,0 +1,10 @@
+﻿using Organizador_PEC_6_60.Instrumento.Domain.ValueObjects;
+
+namespace Organizador_PEC_6_60.Instrumento.Application.Create;
+
+public interface InstrumentoCreatorService
+{
+    void Create(InstrumentoAñoEstadistico añoEstadistico, InstrumentoMesEstadistico mesEstadistico,
+        InstrumentoConsecutivo consecutivo, byte[] dataArchivo, int idInstrumento, int idTipoEstadistica,
+        int idMunicipio);
+}
